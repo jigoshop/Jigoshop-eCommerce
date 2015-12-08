3460a3d91000958851c80f32340724b54181a465
Tomasz Dyda <tomsz.dyda@borbis.pl>
2015-12-08 09:55:54 +0100

    -- form tag

--- templates/admin/migration.php
+++ templates/admin/migration.php
@@ -13,14 +13,12 @@ use Jigoshop\Helper\Render;
 	<p class="alert alert-info"><?php _e('This panel allows you to update your old Jigoshop plugin data to new format.', 'jigoshop'); ?></p>
 	<p class="alert alert-info"><?php _e('Migration is a lengthy process and depends on how much items you have in your store. Please keep patient until the process is finished.', 'jigoshop'); ?></p>
 	<p class="alert alert-danger no-remove"><?php printf(__('Please create a backup of database in case of any error! <a href="%s">Here you can find instruction how to do this</a>', 'jigoshop'), 'http://codex.wordpress.org/Backing_Up_Your_Database'); ?></p>
-<!--	<form action="" method="get">-->
-		<ul class="list-group clearfix max-width-250">
-			<?php foreach ($tools as $tool): /** @var $tool \Jigoshop\Admin\Migration\Tool */ ?>
-				<li class="list-group-item tool-<?php echo $tool->getId(); ?>"><?php echo $tool->display(); ?></li>
-			<?php endforeach; ?>
-		</ul>
-		<input type="hidden" name="page" value="<?php echo Jigoshop\Admin\Migration::NAME; ?>" />
-<!--	</form>-->
+	<ul class="list-group clearfix max-width-250">
+		<?php foreach ($tools as $tool): /** @var $tool \Jigoshop\Admin\Migration\Tool */ ?>
+			<li class="list-group-item tool-<?php echo $tool->getId(); ?>"><?php echo $tool->display(); ?></li>
+		<?php endforeach; ?>
+	</ul>
+	<input type="hidden" name="page" value="<?php echo Jigoshop\Admin\Migration::NAME; ?>" />
 </div>
 <div class="wrap jigoshop migration_progress hidden">
 	<h1 id="title"><?php _e('Jigoshop &raquo; Migration Tool &raquo; ', 'jigoshop'); ?></h1>
