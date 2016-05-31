c54f5f254ff666c2f01f7d820d0d0771d56071e9
Krzysztof Kasowski <krzysztof@kasowski.net>
2016-05-31 10:13:19 +0200

    ~~ fix tragic code

--- templates/shop/product/images.php
+++ templates/shop/product/images.php
@@ -14,7 +14,7 @@ use Jigoshop\Helper\Product;
 		<span class="on-sale"><?php _e('Sale!', 'jigoshop'); ?></span>
 	<?php endif; ?>
 	<?php do_action('jigoshop\template\product\before_featured_image', $product); ?>
-	<a href="<?php echo $featuredUrl; ?>" class="<?php $imageClasses[] = 'featured-image'; echo join(' ', $imageClasses); ?>" data-lightbox="product-gallery"><?php echo $featured; ?></a>
+	<a href="<?php echo $featuredUrl; ?>" class="<?php echo join(' ', $imageClasses); ?>" data-lightbox="product-gallery"><?php echo $featured; ?></a>
 	<?php do_action('jigoshop\template\product\before_thumbnails', $product); ?>
 	<div class="thumbnails">
 		<?php foreach($thumbnails as $id => $thumbnail): ?>
