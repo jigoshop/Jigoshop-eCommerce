80263b43036b0882910774e3c7f110ae000c0c13
ciastek <92maciaszek.maciej@gmail.com>
2017-03-14 12:53:52 +0100

    ~~#351 - Api documentation - Orders cleaning

--- src/Jigoshop/Api/Routes/V1/Orders.php
+++ src/Jigoshop/Api/Routes/V1/Orders.php
@@ -25,52 +25,6 @@ class Orders extends PostController implements ApiControllerContract
     protected $app;
 
     /**
-     * basic method to GET all orders
-     * @param Request $request
-     * @param Response $response
-     * @param $args
-     * @return Response
-     */
-    public function getOrders(Request $request, Response $response, $args)
-    {
-        if (!$this->app->getContainer()->token->hasPermission(Permission::READ_ORDERS)) {
-            throw new Exception('You have no permissions to access to this page.', 403);
-        }
-
-        /** @var OrderService $service */
-        $service = $this->app->getContainer()->di->get('jigoshop.service.order');
-        $queryParams = $request->getParams();
-        $queryParams['pagelen'] = isset($queryParams['pagelen']) && is_numeric($queryParams['pagelen']) ? (int)$queryParams['pagelen'] : 10;
-        $queryParams['page'] = isset($queryParams['page']) && is_numeric($queryParams['page']) ? (int)$queryParams['page'] : 1;
-        $allOrders = 12;// $service->getOrdersCount();
-
-        $orders = $service->findByQuery(new \WP_Query([
-            'post_type' => Types::ORDER,
-            'posts_per_page' => $queryParams['pagelen'],
-            'paged' => $queryParams['page'],
-            'post_status' => [
-                OrderEntity\Status::CANCELLED,
-                OrderEntity\Status::COMPLETED,
-                OrderEntity\Status::PROCESSING,
-                OrderEntity\Status::PENDING,
-                OrderEntity\Status::REFUNDED,
-                OrderEntity\Status::ON_HOLD
-            ]
-        ]));
-
-        return $response->withJson([
-            'success' => true,
-            'all_results' => $allOrders,
-            'pagelen' => $queryParams['pagelen'],
-            'page' => $queryParams['page'],
-            'next' => '',
-            'previous' => '',
-            'data' => array_values($orders),
-        ]);
-
-    }
-
-    /**
      * @apiDefine OrderReturnObject
      * @apiSuccess {Number}    data.id    The ID.
      * @apiSuccess {Number}    data.number    Ordering number.
@@ -251,6 +205,51 @@ class Orders extends PostController implements ApiControllerContract
          * @apiUse validateObjectFindingError
          */
         $app->delete('/{id:[0-9]+}', array($this, 'delete'));
+    }
+
+    /**
+     * basic method to GET all orders
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return Response
+     */
+    public function getOrders(Request $request, Response $response, $args)
+    {
+        if (!$this->app->getContainer()->token->hasPermission(Permission::READ_ORDERS)) {
+            throw new Exception('You have no permissions to access to this page.', 403);
+        }
+
+        /** @var OrderService $service */
+        $service = $this->app->getContainer()->di->get('jigoshop.service.order');
+        $queryParams = $request->getParams();
+        $queryParams['pagelen'] = isset($queryParams['pagelen']) && is_numeric($queryParams['pagelen']) ? (int)$queryParams['pagelen'] : 10;
+        $queryParams['page'] = isset($queryParams['page']) && is_numeric($queryParams['page']) ? (int)$queryParams['page'] : 1;
+        $allOrders = 12;// $service->getOrdersCount();
+
+        $orders = $service->findByQuery(new \WP_Query([
+            'post_type' => Types::ORDER,
+            'posts_per_page' => $queryParams['pagelen'],
+            'paged' => $queryParams['page'],
+            'post_status' => [
+                OrderEntity\Status::CANCELLED,
+                OrderEntity\Status::COMPLETED,
+                OrderEntity\Status::PROCESSING,
+                OrderEntity\Status::PENDING,
+                OrderEntity\Status::REFUNDED,
+                OrderEntity\Status::ON_HOLD
+            ]
+        ]));
+
+        return $response->withJson([
+            'success' => true,
+            'all_results' => $allOrders,
+            'pagelen' => $queryParams['pagelen'],
+            'page' => $queryParams['page'],
+            'next' => '',
+            'previous' => '',
+            'data' => array_values($orders),
+        ]);
     }
 
     /**
