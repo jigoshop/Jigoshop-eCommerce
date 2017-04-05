83e3f00d7865eaae3c42da12dbd23983a96d1316
Krzysztof Kasowski <krzysztof@kasowski.net>
2017-04-05 12:55:18 +0200

    ~~ fix downloadable product link in order email

--- src/Jigoshop/Core/Types/Product/Downloadable.php
+++ src/Jigoshop/Core/Types/Product/Downloadable.php
@@ -185,6 +185,10 @@ class Downloadable implements Type
 	public function emailLink($result, $item, $order)
 	{
 		$product = $item->getProduct();
+		if($product instanceof Product\Variable) {
+		    $product = $product->getVariation($item->getMeta('variation_id')->getValue());
+        }
+
 		if ($product instanceof Product\Downloadable && in_array($order->getStatus(), array(Order\Status::COMPLETED, Order\Status::PROCESSING))) {
 			$url = $this->wp->getHelpers()->addQueryArg(array('file' => $order->getKey().'.'.$order->getId().'.'.$item->getKey()), Endpoint::getUrl(DownloadFile::NAME));
 			$result .= PHP_EOL.__('Your download link for this file is:', 'jigoshop');
