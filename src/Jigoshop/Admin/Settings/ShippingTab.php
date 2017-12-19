12b5f3d2d2bd643302dfa1d0070e882f9548d586
Marcin Kowalak <mkconel@gmail.com>
2017-12-19 08:57:20 +0100

    Fixed issue with not showing specific countries select when Flat Rate is set to "selected countries".

--- src/Jigoshop/Admin/Settings/ShippingTab.php
+++ src/Jigoshop/Admin/Settings/ShippingTab.php
@@ -42,6 +42,10 @@ class ShippingTab implements TabInterface
             	    ['jquery', 'wp-util', 'jquery-ui-sortable'], 
                     ['page' => 'jigoshop_page_jigoshop_settings']);
 
+            	Scripts::add('jigoshop.admin.settings.shipping.flat_rate', \JigoshopInit::getUrl() . '/assets/js/admin/settings/shipping/flat_rate.js', 
+            	    ['jquery', 'wp-util', 'jquery-ui-sortable'], 
+                    ['page' => 'jigoshop_page_jigoshop_settings']);            	
+
                 Scripts::add('jigoshop.magnific-popup', \JigoshopInit::getUrl() . '/assets/js/vendors/magnific_popup.js', ['jquery']);
                 
                 Styles::add('jquery-ui-sortable');
