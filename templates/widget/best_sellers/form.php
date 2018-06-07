d8520570be0d9e0a4bfe2a00eb2bdc6a2b4018c6
Krzysztof Kasowski <krzysztof@kasowski.net>
2018-06-07 10:26:37 +0200

    add possibility to change style

--- templates/widget/best_sellers/form.php
+++ templates/widget/best_sellers/form.php
@@ -6,6 +6,8 @@
  * @var $number_id string Number field ID.
  * @var $number_name string Number field name.
  * @var $number string Number of products in widget.
+ * @var $style_id string Style field ID.
+ * @var $style_name string
  */
 ?>
 <p>
@@ -16,3 +18,11 @@
 	<label for="<?= $number_id; ?>"><?php _e('Number of products to show:', 'jigoshop-ecommerce'); ?></label>
 	<input class="widefat" id="<?= $number_id; ?>"  name="<?= $number_name; ?>" type="number" min="1" value="<?= $number; ?>" />
 </p>
+<p>
+    <label for="<?= $style_id; ?>"><?php _e('Style:', 'jigoshop-ecommerce'); ?></label>
+    <select class="widefat" id="<?= $style_id; ?>" name="<?= $style_name; ?>">
+        <option value="compact" <?php selected('compact', $style); ?>><?= __('Compact', 'jigoshop-ecommerce'); ?></option>
+        <option value="full" <?php selected('full', $style); ?>><?= __('Full', 'jigoshop-ecommerce'); ?></option>
+    </select>
+</p>
+
