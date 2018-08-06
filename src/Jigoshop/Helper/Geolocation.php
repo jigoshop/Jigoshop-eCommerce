9f23ba43cba40457bdd79412ff0091c157742cc1
Marcin Kowalak <mkconel@gmail.com>
2018-08-06 11:15:26 +0200

    Added geolocation database file.

--- src/Jigoshop/Helper/Geolocation.php
+++ src/Jigoshop/Helper/Geolocation.php
@@ -1,10 +1,11 @@
 <?php
 namespace Jigoshop\Helper;
 
+use JigoshopInit;
 use Jigoshop\Exception;
 
 class Geolocation {
-	const DB_PATH = 'source-filtered.csv';
+	const DB_PATH = 'assets/other/geolocation.csv';
 
 	/**
 	 * Returns country code of specified IP address.
@@ -16,14 +17,14 @@ class Geolocation {
 	 * @return mixed String (country code) if address found, null if not.
 	 */
 	public static function getCountryOfIP($ipAddress) {
-		if(!file_exists(self::DB_PATH) || !is_readable(self::DB_PATH)) {
+		if(!file_exists(self::getDbPath()) || !is_readable(self::getDbPath())) {
 			throw new Exception(__('Unable to read geolocation database.', 'jigoshop-ecommerce'));
 		}
 
 		$decimal = self::ipToDecimal($ipAddress);
 
 		$country = null;
-		$db = fopen(self::DB_PATH, 'r');
+		$db = fopen(self::getDbPath(), 'r');
 		while(!feof($db)) {
 			$line = trim(fgets($db));
 			if(!$line) {
@@ -40,6 +41,15 @@ class Geolocation {
 		fclose($db);
 
 		return $country;
+	}
+
+	/**
+	 * Returns complete path to location database file.
+	 * 
+	 * @return string Database path.
+	 */
+	private static function getDbPath() {
+		return sprintf('%s/%s', JigoshopInit::getDir(), self::DB_PATH);
 	}
 
 	/**
